d3367e7216d36c63febace55ed881bc0743435d5
Filip Brühl <filip.h.bruehl@student.fh-kiel.de>
2018-12-06 15:15:14 +0100

    Init Commit

---
+++ Assets/ScoreController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class ScoreController : MonoBehaviour {
+    public int Score;
+    public AudioClip clip;
+
+	// Use this for initialization
+	void Start () {
+        Score = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Hit")
+        {
+            Score++;
+            Debug.Log("Collision with: " + collision.collider.name);
+            AudioSource.PlayClipAtPoint(clip, collision.transform.position, 1.5f);
+        }
+    }
+}
